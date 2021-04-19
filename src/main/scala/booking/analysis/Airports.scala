d76332f64ff10c481970715b22fed46b632e112b
Ozgun Alan <losthimself@gmail.com>
2021-04-19 17:43:39 +0200

    refactor: moved reading airports from worksheet to its class

---
+++ src/main/scala/booking/analysis/Airports.scala
@@ -0,0 +1,39 @@
+package booking.analysis
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.col
+
+object Airports {
+
+  case class LocationInfo(iata: String, country: String, tz: String)
+
+  def toLookup(spark: SparkSession, path: String): Map[String, (String, String)] = {
+    import spark.implicits._
+    spark.read
+      .format("csv")
+      .option("header", "false")
+      .option("inferSchema", "true")
+      .load(path)
+      .toDF("id"
+        ,"name"
+        ,"city"
+        ,"country"
+        ,"iata"
+        ,"icao"
+        ,"lat"
+        ,"lon"
+        ,"alt"
+        ,"timezone"
+        ,"dst"
+        ,"tz"
+        ,"airportType"
+        ,"source")
+      .select("iata", "country", "tz")
+      .filter(col("iata") =!= "\\N")
+      .filter(col("tz") =!= "\\N")
+      .as[LocationInfo]
+      .collect()
+      .map(a => a.iata -> (a.country, a.tz))
+      .toMap
+  }
+}
---
+++ src/main/scala/booking/analysis/Bookings.scala
@@ -0,0 +1,5 @@
+package booking.analysis
+
+object Bookings {
+
+}
